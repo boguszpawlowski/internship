02e1fa6247184477af0d9cee63fef26ca21da5e4
Kacper <kacperdobosz2000@gmail.com>
2021-05-17 22:19:38 +0200

    final

--- app/src/main/java/com/example/fibbonacci/Fibb.kt
+++ app/src/main/java/com/example/fibbonacci/Fibb.kt
@@ -2,7 +2,7 @@ package com.example.fibbonacci
 
 
 fun main() {
-    val n = 7
+    val n = 5
     var x = 0
     var y = 1
     var suma = 0
@@ -14,6 +14,4 @@ fun main() {
     }
     println("n = $n")
     println("Oto wyraz ciagu: $x")
-
-    println("Zmiana 1")
 }
