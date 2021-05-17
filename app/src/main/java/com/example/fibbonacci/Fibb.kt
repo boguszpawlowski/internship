8c3d2b18a849e1456e6d479b1d8ad617863bc10b
Kacper <kacperdobosz2000@gmail.com>
2021-05-17 18:11:15 +0200

    Druga zmiana

--- app/src/main/java/com/example/fibbonacci/Fibb.kt
+++ app/src/main/java/com/example/fibbonacci/Fibb.kt
@@ -1,5 +1,17 @@
 package com.example.fibbonacci
 
-fun main(){
-    println("test")
+
+fun main() {
+    val n = 8
+    var x = 0
+    var y = 1
+    var suma = 0
+
+    for (i in 1..n) {
+        suma = x + y
+        x = y
+        y = suma
+    }
+    println("n = $n")
+    println("Oto wyraz ciagu: $x")
 }
